7f9681dc451c90aed5c282f566d6001d21a1f791
Swagnik02 <swagnik1234@gmail.com>
2024-08-15 23:47:44 +0530

    docModel working

--- lib/ui/home/doc_card.dart
+++ lib/ui/home/doc_card.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:docibry/models/document_model.dart';
 import 'package:flutter/material.dart';
 
@@ -11,18 +12,23 @@ class DocCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.only(top: 16, right: 16, left: 16),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondaryContainer,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      width: double.infinity,
-      child: Text(
-        docModel.docName,
-        style: const TextStyle(color: Colors.black, fontSize: 30),
+    return GestureDetector(
+      onTap: () {
+        log(docModel.toMap().toString());
+      },
+      child: Container(
+        height: 150,
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.only(top: 16, right: 16, left: 16),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        width: double.infinity,
+        child: Text(
+          docModel.docName,
+          style: const TextStyle(color: Colors.black, fontSize: 30),
+        ),
       ),
     );
   }
